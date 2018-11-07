5683915e3e6da1f2740394b8522cc7793ce78da7
Hardin253874 <hardin.253874@gmail.com>
2018-11-08 08:36:48 +1100

    Initial Project

---
+++ app/app.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        // Angular modules 
+        'ngRoute'         // routing
+    ]);
+
+    // Handle routing events
+    app.run( [ '$route', '$rootScope', '$http',, run]);
+
+    function run( $route, $rootScope, $http) {
+
+    }
+
+})();
